73fb2b40fb3619981b6208f5f0dea506e62e83bc
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-14 22:31:35 +0100

    add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
 
 function routes(app: Express) {
 
     app.get('/healthcheck', (req: Request, res: Response) => {
         return res.sendStatus(200)
     })
+
+    app.post('/api/users', createUserHandler)
 }
 
 
