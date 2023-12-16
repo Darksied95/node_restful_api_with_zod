6fb80900457f9271f2b6b4a4b3e59dae040be182
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-16 12:11:04 +0100

    add validate schema resources

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
+import validateResources from "./middleware/validateResources";
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
 
@@ -7,7 +9,7 @@ function routes(app: Express) {
         return res.sendStatus(200)
     })
 
-    app.post('/api/users', createUserHandler)
+    app.post('/api/users', validateResources(createUserSchema), createUserHandler)
 }
 
 
