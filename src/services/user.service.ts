27c45b7065f7dd95804b992071b4f5ab4c7b9fd4
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-16 13:01:20 +0100

    add validataePassword function

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,4 @@
+import { omit } from "lodash";
 import UserModel, { UserInput } from "../models/user.model";
 
 export async function createUser(input: UserInput) {
@@ -6,4 +7,16 @@ export async function createUser(input: UserInput) {
     } catch (e: any) {
         throw new Error(e)
     }
+}
+
+export async function validatePassword({ email, password }: { email: string, password: string }) {
+    const user = await UserModel.findOne({ email })
+    if (!user) return false
+
+    const isPasswordValid = user.comparePassword(password)
+
+    if (!isPasswordValid) return false
+
+    return omit(user.toJSON(), "password")
+
 }
