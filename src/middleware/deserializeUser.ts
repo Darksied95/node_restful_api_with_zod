09ef30de57c1b48d91cd94a2972760fcbc8d1dbc
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-17 22:51:53 +0100

    add deserializeUser  middleware

---
+++ src/middleware/deserializeUser.ts
@@ -0,0 +1,13 @@
+import { Response, Request, NextFunction } from 'express';
+import { get } from 'lodash';
+import { verifyJwt } from '../utils/jwt.utils';
+export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+    const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "")
+
+    const { decoded, expired } = verifyJwt(accessToken)
+    if (decoded) {
+        res.locals.user = decoded
+    }
+
+    return next()
+} 
