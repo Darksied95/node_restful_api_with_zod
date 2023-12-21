d2a316672f632f7787012b02ece70ff46e76cc8c
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-21 11:11:48 +0100

    add sessions routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Express, Request, Response } from "express";
-import { createUserHandler } from "./controller/user.controller";
 import validateResources from "./middleware/validateResources";
+import { createUserHandler } from "./controller/user.controller";
+import { createUserSessionHandler, getUserSessionHandler } from "./controller/session.controller";
 import { createUserSchema } from "./schema/user.schema";
-
+import { createSessionSchema } from "./schema/session.schema";
 function routes(app: Express) {
 
     app.get('/healthcheck', (req: Request, res: Response) => {
@@ -10,6 +11,9 @@ function routes(app: Express) {
     })
 
     app.post('/api/users', validateResources(createUserSchema), createUserHandler)
+    app.post('/api/sessions', validateResources(createSessionSchema), createUserSessionHandler)
+    app.get("/api/sessions", getUserSessionHandler)
+
 }
 
 
