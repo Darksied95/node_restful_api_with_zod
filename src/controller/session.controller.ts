31570baa1a928e216b038e260c216ddd7a4ad463
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-21 11:11:26 +0100

    add get userSessionHandler

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from "express"
 import { validatePassword } from "../services/user.service"
-import { createSession } from "../services/session.service"
+import { createSession, findSessions } from "../services/session.service"
+import { signJwt } from "../utils/jwt.utils"
+import config from 'config';
 
 export async function createUserSessionHandler(req: Request, res: Response) {
     const user = await validatePassword(req.body)
 
     if (!user) return res.status(401).send("Invalid email or password")
 
-    const session = createSession(user._id, req.get("user_agent") || "")
+    const session = await createSession(user._id, req.get("user_agent") || "")
+
+    const accessToken = signJwt(
+        { ...user, session: session._id }, { expiresIn: config.get("accessTokenTtl") }
+    )
+
+    const refreshToken = signJwt(
+        { ...user, session: session._id }, { expiresIn: config.get("refreshTokenTtl") }
+    )
+
+    return res.json({ accessToken, refreshToken })
+}
+
+export async function getUserSessionHandler(req: Request, res: Response) {
+    const userId = res.locals.user._id
+    const sessions = await findSessions({ user: userId, valid: true })
+
+    return res.send(sessions)
 }
