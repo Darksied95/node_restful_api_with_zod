e9750120ff597441a09c700cdd7f120667f6d743
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-23 20:09:08 +0100

    reissue accessToken if refreshToken is valid

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,12 +1,41 @@
 import { Response, Request, NextFunction } from 'express';
 import { get } from 'lodash';
 import { verifyJwt } from '../utils/jwt.utils';
-export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+import { reIssueAccessToken } from '../services/session.service';
+
+
+export const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "")
+    const refreshToken = get(req, "headers.x-refresh")
+
+    if (!accessToken) return next()
+
 
     const { decoded, expired } = verifyJwt(accessToken)
+
+
     if (decoded) {
         res.locals.user = decoded
+        return next()
+    }
+
+    if (expired && refreshToken) {
+        const newAccessToken = await reIssueAccessToken(refreshToken as string)
+        console.log(3);
+        console.log(newAccessToken);
+
+        if (!newAccessToken) {
+            return next()
+        }
+
+        res.setHeader("x-access-token", newAccessToken)
+
+        const result = verifyJwt(newAccessToken)
+
+        res.locals.user = result.decoded
+        console.log(4);
+
+        return next()
     }
 
     return next()
