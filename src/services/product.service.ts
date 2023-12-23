0aa854d105a2005c0cba36e8d1d128586f7f5b91
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-23 20:34:05 +0100

    add findAndUpdateProduct service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, QueryOptions } from "mongoose";
+import { FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
 import ProductModel, { ProductDocument, ProductInput } from "../models/product.model";
 
 export async function createProduct(input: ProductInput) {
@@ -8,3 +8,8 @@ export async function createProduct(input: ProductInput) {
 export async function findProduct(query: FilterQuery<ProductDocument>, options: QueryOptions = { lean: true }) {
     return await ProductModel.findOne(query, {}, options)
 }
+
+export async function findAndUpdateProduct(query: FilterQuery<ProductDocument>, update: UpdateQuery<ProductDocument>, options: QueryOptions) {
+    return await ProductModel.findOneAndUpdate(query, update, options)
+}
+
