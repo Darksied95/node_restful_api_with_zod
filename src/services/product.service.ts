1ac8624115d5b04dc7e68b79b891e9e9ab423daf
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-23 20:31:33 +0100

    add findProduct service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -4,3 +4,7 @@ import ProductModel, { ProductDocument, ProductInput } from "../models/product.m
 export async function createProduct(input: ProductInput) {
     return await ProductModel.create(input)
 }
+
+export async function findProduct(query: FilterQuery<ProductDocument>, options: QueryOptions = { lean: true }) {
+    return await ProductModel.findOne(query, {}, options)
+}
